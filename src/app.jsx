64878da47a442988a8045c78edc95cfc4d4e4051
odaepo <odaepo@gmail.com>
2016-05-18 17:56:23 +0200

    allineamento

--- src/app.jsx
+++ src/app.jsx
@@ -6,6 +6,70 @@ var Modulo = require('./modulo');
 //var Header = require('./header');
 //var List = require('./list');
 
+
+
+
+
+var startWithTables=['TA','TB','TC'];
+
+var relationsTree={};
+
+relationsTree= {
+    'TA':{
+        'data':'...stuff TA ...',
+        'related': ['TD','TE','TF']
+    },
+    'TB':{
+        'data':'...stuff TB ...',
+        'related': ['TG','TH']
+    }
+    ,
+    'TC':{
+        'data':'...stuff C ...',
+        'related': {}
+    },
+    'TD':{
+        'data':'...stuff TD ...',
+        'related': ['TI','TL']
+    },
+    'TE':{
+        'data':'...stuff TE ...',
+        'related': ['TM']
+    },
+    'TF':{
+        'data':'...stuff TF ...',
+        'related': []
+    },
+    'TG':{
+        'data':'...stuff TG ...',
+        'related': []
+    },
+    'TH':{
+        'data':'...stuff TH ...',
+        'related': []
+    },
+    'TI':{
+        'data':'...stuff TI ...',
+        'related': ['TN']
+    },
+    'TL':{
+        'data':'...stuff TL ...',
+        'related': []
+    },
+    'TM':{
+        'data':'...stuff TM ...',
+        'related': []
+    },
+    'TN':{
+        'data':'...stuff TN ...',
+        'related': ['TA']
+    }
+};
+
+
+
+
+
 var mainInfoTree=[3,4,5];
 
 var Hello = React.createClass({
@@ -35,3 +99,15 @@ var Hello = React.createClass({
 
 var element = React.createElement(Hello, {});
 ReactDOM.render(element, document.querySelector('.container'));
+
+
+
+
+
+
+
+
+
+
+
+ 
